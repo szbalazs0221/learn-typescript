091f2c2c673901210cd93b1ba1ba07593d9587b1
szbalazs3 <szabo.balazs0221@gmail.com>
2024-03-01 22:45:25 +0100

    sort implemented for numbers

---
+++ sort/src/index.ts
@@ -0,0 +1,24 @@
+class Sorter {
+  collection: number[];
+
+  constructor(collection: number[]) {
+    this.collection = collection;
+  }
+
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftElement = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftElement;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0, 100, -100]);
+sorter.sort();
+console.log(sorter.collection);
