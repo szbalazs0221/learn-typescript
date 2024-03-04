ea1a3e426320044c7274260d40082001dee71c09
szbalazs3 <szabo.balazs0221@gmail.com>
2024-03-04 21:16:08 +0100

    write first report

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log('Hello');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default
+    .readFileSync('football.csv', { encoding: 'utf-8' })
+    .split('\n')
+    .map((row) => {
+    return row.split(',');
+});
+let manUnitedWins = 0;
+for (let match of matches) {
+    const homeTeam = match[1];
+    const awayTeam = match[2];
+    const winner = match[5];
+    if ((homeTeam === 'Man United' && winner === 'H') ||
+        (awayTeam === 'Man United' && winner === 'A')) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  const homeTeam = match[1];
+  const awayTeam = match[2];
+  const winner = match[5];
+  if (
+    (homeTeam === 'Man United' && winner === 'H') ||
+    (awayTeam === 'Man United' && winner === 'A')
+  ) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
