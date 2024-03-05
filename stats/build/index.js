9196537ab98712bef9673e2d110988bd6eef65ad
szbalazs3 <szabo.balazs0221@gmail.com>
2024-03-05 06:20:02 +0100

    introduce CsvFileReader

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,22 +1,21 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default
-    .readFileSync('football.csv', { encoding: 'utf-8' })
-    .split('\n')
-    .map((row) => {
-    return row.split(',');
-});
+const CsvFileReader_1 = require("./CsvFileReader");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
 let manUnitedWins = 0;
-for (let match of matches) {
+for (let match of reader.data) {
     const homeTeam = match[1];
     const awayTeam = match[2];
     const winner = match[5];
-    if ((homeTeam === 'Man United' && winner === 'H') ||
-        (awayTeam === 'Man United' && winner === 'A')) {
+    if ((homeTeam === 'Man United' && winner === MatchResult.HomeWin) ||
+        (awayTeam === 'Man United' && winner === MatchResult.AwayWin)) {
         manUnitedWins++;
     }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,11 +1,7 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', { encoding: 'utf-8' })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 enum MatchResult {
   HomeWin = 'H',
@@ -15,7 +11,7 @@ enum MatchResult {
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   const homeTeam = match[1];
   const awayTeam = match[2];
   const winner = match[5];
