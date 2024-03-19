3a413a976e7e2e77f29c42c1a5d85ab4a7c1bc5f
szbalazs3 <szabo.balazs0221@gmail.com>
2024-03-19 07:29:35 +0100

    add nested views

--- web/src/views/UserEdit.ts
+++ web/src/views/UserEdit.ts
@@ -1,4 +1,6 @@
 import { User, UserProps } from '../models/User';
+import { UserForm } from './UserForm';
+import { UserShow } from './UserShow';
 import { View } from './View';
 
 export class UserEdit extends View<User, UserProps> {
@@ -7,6 +9,11 @@ export class UserEdit extends View<User, UserProps> {
       userShow: '.user-show',
       userForm: '.user-form',
     };
+  }
+
+  onRender(): void {
+    new UserShow(this.regions.userShow, this.model).render();
+    new UserForm(this.regions.userForm, this.model).render();
   }
 
   template(): string {
--- web/src/views/View.ts
+++ web/src/views/View.ts
@@ -47,12 +47,15 @@ export abstract class View<T extends Model<K>, K extends HasId> {
     }
   }
 
+  onRender(): void {}
+
   render(): void {
     this.parent.innerHTML = '';
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
     this.bindEvents(templateElement.content);
     this.mapRegions(templateElement.content);
+    this.onRender();
     this.parent.append(templateElement.content);
   }
 }
