e18da7c465b437f91aaea312cb06f816731a0fb6
szbalazs0221 <szabo.balazs0221@gmail.com>
2024-03-25 16:09:37 +0100

    add logError decorator

---
+++ features/decorators.ts
@@ -0,0 +1,26 @@
+class Boat {
+  color: string = 'red';
+
+  get formattedColor(): string {
+    return `This boats color is ${this.color}`;
+  }
+
+  @logError
+  pilot(): void {
+    throw new Error();
+    console.log('swish');
+  }
+}
+
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  const method = desc.value;
+  desc.value = function () {
+    try {
+      method();
+    } catch (e) {
+      console.log('Oops, boat was sunk');
+    }
+  };
+}
+
+new Boat().pilot()
